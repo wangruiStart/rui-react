8ab0ad1ec4b087d298328aa0042ed63bbb77aa19
wangrui <412285675@qq.com>
2023-08-15 14:04:39 +0800

    feat: 更换调试方式

---
+++ scripts/vite/vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import replace from '@rollup/plugin-replace';
+import { resolvePackagePaths } from '../rollup/utils';
+import path from 'path';
+
+export default defineConfig({
+	plugins: [
+		react(),
+		replace({
+			__DEV__: true
+		})
+	],
+	resolve: {
+		alias: [
+			{
+				find: 'react',
+				replacement: resolvePackagePaths('react')
+			},
+			{
+				find: 'react-dom',
+				replacement: resolvePackagePaths('react-dom')
+			},
+			{
+				find: 'hostConfig',
+				replacement: path.resolve(
+					resolvePackagePaths('react-dom'),
+					'./src/hostConfig.ts'
+				)
+			}
+		]
+	}
+});
